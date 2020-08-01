e8ee309a96c1e054df4e314ee749e6e6aa08ff69
shabeerv <shabrv@gmail.com>
2020-08-01 16:02:38 +0530

    minor update

--- config.php
+++ config.php
@@ -25,12 +25,12 @@
 
     if(isset($_POST['sendMessage']))
     {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $message = $_POST['message'];
+        $uname = $_POST['name'];
+        $uemail = $_POST['email'];
+        $uphone = $_POST['phone'];
+        $umessage = $_POST['message'];
 
-        $query =  "INSERT INTO contact(uname, uemail, uphone, umessage) VALUES ('$name', '$email', '$phone', '$message')";
+        $query =  "INSERT INTO contact(uname, uemail, uphone, umessage) VALUES ('$uname', '$uemail', '$uphone', '$umessage')";
         $result = mysqli_query($conn, $query);
 
         if($result)
