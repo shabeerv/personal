9c573abee12fef03921795068010fef413043a2f
shabeerv <shabrv@gmail.com>
2020-08-01 16:37:42 +0530

    minor update

--- config.php
+++ config.php
@@ -1,40 +1,37 @@
 <?php
+
     $host = "localhost";
     $username = "shabeer";
     $password = "12345678";
     $dbname = "shabeervco";
 
-    /*try{
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "success";!
-    }catch(PDOException $e){
-        echo "Connection failed: ".$e->getMessage();
-    }*/
-
     $conn = new mysqli($host, $username, $password, $dbname);
 
     // Check connection
-    if ($conn->connect_error) {
+    if ($conn->connect_error)
+    {
         die("Connection failed: " . $conn->connect_error);
-     }
+    }
 
     if(isset($_POST['sendMessage']))
     {
-        $uname = $_POST['name'];
-        $uemail = $_POST['email'];
-        $uphone = $_POST['phone'];
-        $umessage = $_POST['message'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
 
-        $query =  "INSERT INTO contact(uname, uemail, uphone, umessage) VALUES ('$uname', '$uemail', '$uphone', '$umessage')";
+        $query =  "INSERT INTO contact(name, email, phone, message) VALUES ('$name', '$email', '$phone', '$message')";
         $result = mysqli_query($conn, $query);
 
-        if ($result) {
-            echo "New record created successfully";
-         } else {
-            echo "Error: " . $query . "" . mysqli_error($conn);
-         }
-
+        if ($result) 
+        {
+            header("location:/");
+        } 
+        else 
+        {
+            echo "Error: {$query}". mysqli_error($conn);
+        }
+        $conn->close();
     }
 
 ?>
