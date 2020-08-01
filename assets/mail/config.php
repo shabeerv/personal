3d4c7ab1fd0fceb34dcb0ea8c581b12faf690279
shabeerv <shabrv@gmail.com>
2020-08-01 15:44:00 +0530

    minor update

--- assets/mail/config.php
+++ assets/mail/config.php
@@ -4,11 +4,35 @@
     $password = "12345678";
     $dbname = "shabeervco";
 
-    try{
+    /*try{
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "success";
+        echo "success";!
     }catch(PDOException $e){
         echo "Connection failed: ".$e->getMessage();
+    }*/
+
+    $conn = mysqli_connect($host, $username, $password, $dbname);
+
+    if(isset($_POST['sendMessage']))
+    {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+
+        $query =  "INSERT INTO contact(name, email, phone, message) VALUES ('$name', '$email', '$phone', '$message')";
+        $result = mysqli_query($conn, $query);
+
+        if($result)
+        {
+            echo "success";
+        }
+        else
+        {
+            echo "fail";
+        }
+
     }
+
 ?>
